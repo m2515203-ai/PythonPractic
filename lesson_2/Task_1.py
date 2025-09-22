8d83cef320b8a77c4c52237d21ad5b4815b476ed
Artyom <tema.ru2011@gmail.com>
2025-09-22 20:56:25 +0300

    удалил практику

--- lesson_2/Task_1.py
+++
@@ -1,9 +0,0 @@
-def is_cyclic_shift_efficient(s1, s2):
-    if len(s1) != len(s2):
-        return False
-    return s2 in (s1 + s1)
-
-
-print(is_cyclic_shift_efficient("abcde", "cdeab"))  # True
-print(is_cyclic_shift_efficient("abc", "cdeab"))  # False
-print(is_cyclic_shift_efficient("asdf", "qwerty"))  # True
